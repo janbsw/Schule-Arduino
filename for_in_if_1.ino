603e03b57ffce2be1f0e632c4c6dca539580ec6e
Jan Beisswenger <jan@beisswengers.de>
2021-02-09 18:01:31 +0100

    Update for_in_if_1.ino

--- for_in_if_1.ino
+++ for_in_if_1.ino
@@ -1,6 +1,6 @@
 int ledarray[] = {13, 12, 11, 10, 9};
 int i = 0;
-int  = 5;
+int end = 5;
 
 void setup(){
   	Serial.begin(9600);
@@ -18,7 +18,7 @@ nevereverdothis(i, end);
 void nevereverdothis(int i, int end){ 
 	if(i < end){ 
 		digitalWrite(ledarray[i],HIGH);
-     	delay(100);
+     	delay(200);
      	digitalWrite(ledarray[i],LOW);   
 		nevereverdothis(i+1, end);
       	//Bitte nicht angewöhnen, würde man so nicht machen lg Jan
